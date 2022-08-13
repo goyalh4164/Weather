b13f6282ea66f99c0d80c7bd4f34b6b818843c4b
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-08-13 13:06:52 +0530

    learned about JSON decoding

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -3,6 +3,7 @@ import 'package:weather/services/location.dart';
 import 'package:http/http.dart'
     as http; //----------->we can use by its short name by using "as" keyword
 // import 'package:http/http.dart';
+import 'dart:convert'; //this package is used to decode the JSON as of now from my knowledge
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -34,7 +35,28 @@ class _LoadingScreenState extends State<LoadingScreen> {
         "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=ebe40922610017bb96421256fe31f8b6"));
     //way to be in a secure line
     if (response.statusCode == 200) {
-      print(response.body);
+      String data = response.body;
+      //--------------------just for learning-------------------
+      // print(data);
+      //decoding the JSON
+      //we are traveeling inside the JSON by using the keys as a path
+      // var longitude = jsonDecode(data)['coord']['lon'];
+      //this is how you access elements indide the list
+      // var weatherDescription = jsonDecode(data)['weather'][0]['description'];
+      // print(longitude);
+      //you can find the below path from the extension of the JSON and use it in your own way
+      //weather[0].description
+      // print(weatherDescription);
+      //-------------------just for learning----------------
+      //---------------challenge----------------------
+      var decodedData = jsonDecode(data);
+      //using the above shorthand notation
+      int condition = decodedData['weather'][0]['id'];
+      double temp = decodedData['main']['temp'];
+      String cityName = decodedData['name'];
+      print(condition);
+      print(temp);
+      print(cityName);
     } else {
       print(response.statusCode);
     }
