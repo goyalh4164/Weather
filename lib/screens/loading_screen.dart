c5bc6901e2c46e3d9d9d37d3c11a1293ba62b2ea
harshg77777 <96003201+harshg77777@users.noreply.github.com>
2022-08-13 12:02:02 +0530

    added the http

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:weather/services/location.dart';
-// import 'package:http/http.dart' as http;   ----------->we can use by its short name by using "as" keyword
-import 'package:http/http.dart';
+import 'package:http/http.dart'
+    as http; //----------->we can use by its short name by using "as" keyword
+// import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({Key? key}) : super(key: key);
@@ -29,7 +30,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   Future<void> getData() async {
-    Response response = await get(Uri.parse(
+    http.Response response = await http.get(Uri.parse(
         "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=ebe40922610017bb96421256fe31f8b6"));
     //way to be in a secure line
     if (response.statusCode == 200) {
